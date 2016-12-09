f54d1c41bf8525e4bd1a15c7e03740cfb6d9e270
ernestmachado <ernestmachado@rediffmail.com>
2016-12-09 13:32:16 +0530

    Assignment2_1
    Write a program to print your name without using semicolon in the
    program.

---
+++ Assignment2_1.java
@@ -0,0 +1,9 @@
+package ASSIGNMENTS;
+
+public class Assignment2_1 {
+//Write a program to print your name without using semicolon in the program.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      if (System.out.printf("Ernest Machado\n") == null) {}
+	}
+}
